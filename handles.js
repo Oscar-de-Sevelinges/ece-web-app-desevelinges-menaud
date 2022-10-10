71d2c73aa8be049d966cfab2d8916c02e05921aa
Oscar <oscar.desevelinges@yahoo.fr>
2022-10-10 17:59:50 +0200

    lab 2 complété

--- handles.js
+++ handles.js
@@ -1,5 +1,6 @@
 const url = require('url')
 const qs = require('querystring');
+const aboutOscar = require('./content/about');
 
 module.exports = {
     serverHandle: function (req, res) {
@@ -25,16 +26,28 @@ module.exports = {
 
         res.writeHead(200, {'Content-Type': 'text/plain'})
         if(path === '/') {
-            res.write('In the URL, add /hello?name=yourname to see a personalized message.\nAnother message will be printed with the names of the repository owners (Oscar and Romain)')
+            res.write('In the URL, add /hello?name=yourname to see a personalized message.\n\nAnother message will be printed with the names of the repository owners and contributors (Oscar, Romain and Paul)')
         }
         else if(path === '/hello' && 'name' in params) {
-            res.write('Hello ' + params['name'])
-            if('name' in params === 'Oscar') {
-                res.write('Message for the repository owner Oscar (try with Romain)')
+            if(params['name'] == 'Oscar') {
+                res.write('Message for the repository owner Oscar (try with Romain or Paul)')
             }
-            else if('name' in params === 'Romain') {
-                res.write('Message for the repository owner Romain (try with Oscar)')
+            else if(params['name'] == 'Romain') {
+                res.write('Message for the repository owner Romain (try with Oscar or Paul)')
             }
+            else if(params['name'] == 'Paul') {
+                res.write('Message for the contributor and teacher Paul (try with Oscar or Romain)')
+            }
+            else {
+                res.write('Hello ' + params['name'])
+            }
+        }
+        else if(path === '/hello'){
+            res.write('add a name in the URL as follows: /hello?name=yourname')
+        }
+        else if (path === '/content/aboutOscar') {
+            // display the parsed content of the about.json file
+            res.write(aboutOscar.name + ' is ' + aboutOscar.age + ' years old and lives in ' + aboutOscar.city + '.\n')
         }
         else {
             res.write('404 Page not found')
