c32454f7d0cc951bf0ea2ac50ed357c6a694a23e
OscarUi <oscar.desevelinges@yahoo.fr>
2022-12-16 17:52:57 +0100

    feat: displaying articles

--- app/pages/articles/[slug].js
+++ app/pages/articles/[slug].js
@@ -1,7 +1,19 @@
-export default function Article() {
-    return (
-        <div>
-        <h1>Article</h1>
-        </div>
-    );
+import { useRouter } from 'next/router';
+import Article from '../../components/Article';
+import Layout from '../../components/Layout';
+
+function ArticlePage() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  // Retrieve the data for the specific article based on the slug value
+  const article = getArticleData(slug);
+
+  return (
+    <Layout>
+      <Article article={article} />
+    </Layout>
+  );
 }
+
+export default ArticlePage;
