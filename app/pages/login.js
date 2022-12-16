48891909410461e7770cb2f17e23905c0b21dc21
OscarUi <oscar.desevelinges@yahoo.fr>
2022-12-16 16:05:42 +0100

    feat: core content

---
+++ app/pages/login.js
@@ -0,0 +1,10 @@
+export default function Login() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+          <h1 className="text-4xl font-bold mb-4">Login</h1>
+          <p className="text-xl mb-4">
+            This is the Login page.
+          </p>
+        </div>
+    );
+}
---
+++ app/pages/profile.js
@@ -0,0 +1,10 @@
+export default function Profile() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+          <h1 className="text-4xl font-bold mb-4">Profile</h1>
+          <p className="text-xl mb-4">
+            This is the Profile page.
+          </p>
+        </div>
+    );
+}
