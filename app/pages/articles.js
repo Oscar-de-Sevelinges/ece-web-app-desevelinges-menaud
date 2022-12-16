59de4b32d245ac274697b690c5703177e6a511cd
OscarUi <oscar.desevelinges@yahoo.fr>
2022-12-16 15:20:15 +0100

    fi:; minor update

---
+++ app/pages/articles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Articles() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+          <h1 className="text-4xl font-bold mb-4">Articles</h1>
+          <p className="text-xl mb-4">
+            This is the Articles page.
+          </p>
+        </div>
+    );
+}
